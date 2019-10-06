af68e6b590fff709e2a38f7f122e3b910560e939
adinigufroni <44041905+adinigufroni@users.noreply.github.com>
2019-10-06 23:32:39 +0700

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -11,11 +11,12 @@ profile_target = instaloader.Profile.from_username(C.context, "privat.in_id")
 # print list of followers
 for follower in profile_target.get_followers():
     followers_profile_target = follower.username
-    print("Id : ", followers_profile_target)
+    
 
     profile = instaloader.Profile.from_username(C.context, followers_profile_target)
     #print(type(profile))
-    
+
+#get post's details
     for posts in profile.get_posts():
         post = posts.caption
         #if post is 
@@ -26,6 +27,7 @@ for follower in profile_target.get_followers():
         #for comment in posts.get_comments():
 
         if post is not None:
+            print("Id : ", followers_profile_target)
             print("CAPTION : ", post.encode('ascii', 'ignore'))  #ignore emoticon
             print("HASHTAG : ", hashtag)
             print("LIKE : ", likes)
@@ -33,5 +35,3 @@ for follower in profile_target.get_followers():
             for comment in posts.get_comments():
                 print("COMMENT : " , comment.text.encode('ascii', 'ignore'))
             print("\n")
-
-
